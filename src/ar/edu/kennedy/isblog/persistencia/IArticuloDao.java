b0291416667f687c980eed173679387128ab636b
juancarson <juanignacio@carsonromero.com.ar>
2012-09-22 13:39:03 -0300

    elminacion masiva

--- src/ar/edu/kennedy/isblog/persistencia/IArticuloDao.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.kennedy.isblog.persistencia;
-
-import ar.com.carsonromero.dao.IBaseDao;
-import ar.edu.kennedy.isblog.modelo.Articulo;
-
-public interface IArticuloDao extends  IBaseDao<Articulo> {
-
-}
